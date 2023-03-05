43b3e3f34c420e42b426ba2a563b5aff5fecd1d3
A45935 <93210458+A45935@users.noreply.github.com>
2023-03-05 00:48:40 +0000

    boxView with dropdown

--- app/src/main/java/com/example/regyboxscheduler/login/LoginView.kt
+++ app/src/main/java/com/example/regyboxscheduler/login/LoginView.kt
@@ -1,23 +1,31 @@
 package com.example.regyboxscheduler.login
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.example.regyboxscheduler.ui.SchedulerTheme
 import com.example.regyboxscheduler.ui.TopBar
 
 @Composable
 fun LoginView (
+    boxes: HashMap<String, String>,
     onLoginRequest: (boxId: String, username: String, password: String) -> Unit,
     onInfoRequest: () -> Unit
 ) {
@@ -29,6 +37,18 @@ fun LoginView (
 
     val password = rememberSaveable { mutableStateOf("") }
     val currentPassword = password.value
+
+    var expanded by remember { mutableStateOf(false) }
+
+    // store the selected box
+    var selectedText by remember { mutableStateOf("") }
+
+    var textFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
 
     SchedulerTheme {
         Scaffold(
@@ -45,19 +65,41 @@ fun LoginView (
                     .padding(innerPadding)
                     .fillMaxSize()
             ) {
-                Text(text = "Box Id:", modifier = Modifier.padding(8.dp))
-                Row {
-                    TextField(
-                        value = currentBoxId,
-                        singleLine = true,
-                        colors = TextFieldDefaults.textFieldColors(
-                            backgroundColor = Color(0xFFFFFFFF),
-                            textColor = Color(0xFF000000)
-                        ),
-                        onValueChange = { boxId.value = it }
-                    )
+                Text(text = "Select your box", modifier = Modifier.padding(8.dp))
+
+                OutlinedTextField(
+                    value = selectedText,
+                    onValueChange = { selectedText = it },
+                    modifier = Modifier
+                        .onGloballyPositioned { coordinates ->
+                            textFieldSize = coordinates.size.toSize()
+                        },
+                    trailingIcon = {
+                        Icon(icon, "contentDescription",
+                            Modifier.clickable { expanded = !expanded })
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color(0xFFFFFFFF),
+                        textColor = Color(0xFF000000)
+                    ),
+                    singleLine = true
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                ) {
+                    boxes.forEach { box ->
+                        DropdownMenuItem(onClick = {
+                            selectedText = box.value
+                            expanded = false
+                        }) {
+                            Text(text = box.key)
+                        }
+                    }
                 }
+
                 Text(text = "Username:", modifier = Modifier.padding(8.dp))
+
                 Row {
                     TextField(
                         value = currentUsername,
@@ -69,7 +111,9 @@ fun LoginView (
                         onValueChange = { username.value = it }
                     )
                 }
+
                 Text(text = "Password:", modifier = Modifier.padding(8.dp))
+
                 Row {
                     TextField(
                         value = currentPassword,
@@ -83,6 +127,7 @@ fun LoginView (
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
                     )
                 }
+
                 Row {
                     Button(
                         onClick = { onLoginRequest(currentBoxId, currentUsername, currentPassword) }
@@ -99,6 +144,7 @@ fun LoginView (
 @Composable
 private fun LoginPreview() {
     LoginView(
+        boxes = hashMapOf(),
         onLoginRequest = { _, _, _ -> },
         onInfoRequest = {}
     )
